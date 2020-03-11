eda5c62abd29bd71707d3ac909cbb6c5d0532f0a
mayue-crrc <mayue.1881@163.com>
2020-03-11 21:58:59 +0800

    add runpage items

--- vehiclerunstatepage.cpp
+++ vehiclerunstatepage.cpp
@@ -1,6 +1,10 @@
 #include "vehiclerunstatepage.h"
 #include "ui_vehiclerunstatepage.h"
 
+#define FAULTLEVEL1 "background-color:rgb(240,0,0);color:black;border:transparent;"
+#define FAULTLEVEL2 "background-color:rgb(240,240,0);color:black;border:transparent;"
+#define FAULTLEVEL3 "background-color:rgb(0,0,0);color:rgb(248,248,248);border:transparent;"
+
 VehicleRunStatePage::VehicleRunStatePage(QWidget *parent) :
     MyBase(parent),
     ui(new Ui::VehicleRunStatePage)
@@ -15,9 +19,22 @@ VehicleRunStatePage::~VehicleRunStatePage()
 
 void VehicleRunStatePage::updatePage()
 {
+    ui->LBLFault1->setStyleSheet(FAULTLEVEL1);
+    ui->LBLFault2->setStyleSheet(FAULTLEVEL2);
+    ui->LBLFault3->setStyleSheet(FAULTLEVEL3);
 
 }
 void VehicleRunStatePage::showEvent(QShowEvent *)
+{
+
+}
+
+void VehicleRunStatePage::on_BTNMinus1_clicked()
+{
+
+}
+
+void VehicleRunStatePage::on_BTNPlus1_clicked()
 {
 
 }
